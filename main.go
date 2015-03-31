2a8ab026f86961eab6377a7bd7f8e752ff744440
unknown <tomaszw@POD1558.intware.com>
2015-03-31 15:05:55 -0400

    update

--- main.go
+++ main.go
@@ -1,92 +1,21 @@
 package main
- 
+
 import (
-    "fmt"
-	  "log"
-    "net"
-    "net/mail"
-	  "net/smtp"
-    "crypto/tls"
+        "log"
+        "net/smtp"
 )
- 
-// SSL/TLS Email Example
- 
+
 func main() {
- 
-    from := mail.Address{"", "owoc.ami@gmail.com"}
-    to   := mail.Address{"", "tenrally@gmail.com"}
-    subj := "This is the email subject"
-    body := "This is an example body.\n With two lines."
- 
-    // Setup headers
-    headers := make(map[string]string)
-    headers["From"] = from.String()
-    headers["To"] = to.String()
-    headers["Subject"] = subj
- 
-    // Setup message
-    message := ""
-    for k,v := range headers {
-        message += fmt.Sprintf("%s: %s\r\n", k, v)
-    }
-    message += "\r\n" + body
- 
-    // Connect to the SMTP Server
-    servername := "smtp.gmail.com:465"
- 
-    host, _, _ := net.SplitHostPort(servername)
- 
-    auth := smtp.PlainAuth("","owoc.ami@gmail.com", "haselko123", host)
- 
-    // TLS config
-    tlsconfig := &tls.Config {
-        InsecureSkipVerify: true,
-        ServerName: host,
-    }
- 
-    // Here is the key, you need to call tls.Dial instead of smtp.Dial
-    // for smtp servers running on 465 that require an ssl connection
-    // from the very beginning (no starttls)
-    conn, err := tls.Dial("tcp", servername, tlsconfig)
-    if err != nil {
-        log.Panic(err)
-    }
- 
-    c, err := smtp.NewClient(conn, host)
-    if err != nil {
-        log.Panic(err)
-    }
- 
-    // Auth
-    if err = c.Auth(auth); err != nil {
-        log.Panic(err)
-    }
- 
-    // To && From
-    if err = c.Mail(from.Address); err != nil {
-        log.Panic(err)
-    }
- 
-    if err = c.Rcpt(to.Address); err != nil {
-        log.Panic(err)
-    }
- 
-    // Data
-    w, err := c.Data()
-    if err != nil {
-        log.Panic(err)
-    }
- 
-    _, err = w.Write([]byte(message))
-    if err != nil {
-        log.Panic(err)
-    }
- 
-    err = w.Close()
-    if err != nil {
-        log.Panic(err)
-    }
- 
-    c.Quit()
- 
+	to := "tenrally@gmail.com"
+	from := "lib.reminder.app@gmail.com"
+	pwd := "haselko123"
+	subject := "test"
+	msg := "test email"
+	body := "To: " + to + "\r\nSubject: " + subject + "\r\n\r\n" + msg
+   	auth := smtp.PlainAuth("", from, pwd, "smtp.gmail.com")
+   	err := smtp.SendMail("smtp.gmail.com:587", auth, from,
+   		[]string{to},[]byte(body))
+   	if err != nil {
+    	log.Fatal(err)
+   	}
 }
